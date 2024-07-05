e705b7630fba1ca2dd4dbc9fdcb301f780dc9bee
Laiza Barreto Pires <userlalah2@gmail.com>
2024-07-05 10:34:38 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Info from './components/Info'
+import Service from './components/Service'
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Hero />
+      <Info />
+      <Service />
+    
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Service.jsx
@@ -0,0 +1,22 @@
+export default function () {
+    return(
+        <>
+        <main className="min-h-screen w-full block border-2">
+        <div>
+            <div className="h-auto">
+                <h1>Gain more</h1>
+                <p>With our</p>
+            </div>
+            </div>
+            <div className="">
+                <div className=""></div>
+                <div className=""></div>
+                <div className=""></div>
+                <div className=""></div>
+                <div className=""></div>
+                <div className=""></div>
+            </div>
+        </main>
+        </>
+    )
+}
